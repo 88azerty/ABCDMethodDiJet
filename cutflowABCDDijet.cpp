bfae6b0c2c2c8c61fa9882d12db35822ebaef11f
Hernán Cabañas <hcabanasg@gmail.com>
2017-07-20 14:43:11 -0400

    Dijet mass calculation. Histogram output.

--- cutflowABCDDijet.cpp
+++ cutflowABCDDijet.cpp
@@ -11,6 +11,8 @@ void LLGAnalysis::SetupABCDDijet() {
 	RegionD=0;
 	RegionDWeighted=0;
 
+	makeHist( "mJJSV", 100, 0., 500., "DiJet mass at SV", "Number of Jet Pairs" );
+
 	return;
 }
 
@@ -65,8 +67,58 @@ void LLGAnalysis::ABCDDijetSelection() {
 
 	}
 
-	//check conditions
-	
+	//sort Secondary Vertex jets
+	int idxLeadingJet = -1;
+	double ptLeadingJet = -1.;
+	int idxSubLeadingJet = -1;
+	double ptSubLeadingJet = -1;
+	int idxThirdLeadingJet = -1;
+	double ptThirdLeadingJet = -1;
+	int idxFourthLeadingJet = -1;
+	double ptFourthLeadingJet = -1;
+
+	for (unsigned int iSV = 0; iSV < secVertex_x->size(); iSV++) {
+		for( unsigned int iJToSV = 0; iJToSV < idJetsToSV.at(iSV).size(); ++iJToSV ) {
+			int jIdx = idJetsToSV.at(iSV).at(iJToSV);
+			if( recoJet_pt->at(jIdx).at(SYSJET) > ptLeadingJet ) {
+				idxFourthLeadingJet = idxThirdLeadingJet;
+				ptFourthLeadingJet = ptThirdLeadingJet;
+				idxThirdLeadingJet = idxSubLeadingJet;
+				ptThirdLeadingJet = ptSubLeadingJet;
+				idxSubLeadingJet = idxLeadingJet;
+				ptSubLeadingJet = ptLeadingJet;
+				idxLeadingJet = jIdx;
+				ptLeadingJet = recoJet_pt->at(jIdx).at(SYSJET);
+			}
+			else if ( recoJet_pt->at(jIdx).at(SYSJET) > ptSubLeadingJet ) {
+				idxFourthLeadingJet = idxThirdLeadingJet;
+				ptFourthLeadingJet = ptThirdLeadingJet;
+				idxThirdLeadingJet = idxSubLeadingJet;
+				ptThirdLeadingJet = ptSubLeadingJet;
+				idxSubLeadingJet = jIdx;
+				ptSubLeadingJet = recoJet_pt->at(jIdx).at(SYSJET);
+			}
+			else if ( recoJet_pt->at(jIdx).at(SYSJET) > ptThirdLeadingJet ) {
+				idxFourthLeadingJet = idxThirdLeadingJet;
+				ptFourthLeadingJet = ptThirdLeadingJet;
+				idxThirdLeadingJet = jIdx;
+				ptThirdLeadingJet = recoJet_pt->at(jIdx).at(SYSJET);
+			}
+			else if ( recoJet_pt->at(jIdx).at(SYSJET) > ptFourthLeadingJet ) {
+				idxFourthLeadingJet = jIdx;
+				ptFourthLeadingJet = recoJet_pt->at(jIdx).at(SYSJET);
+			}
+		}
+
+		//Calculate DiJet Mass
+		TLorentzVector p4Jet1, p4Jet2;
+		p4Jet1.SetPtEtaPhiM( recoJet_pt->at(idxLeadingJet).at(SYSJET), recoJet_eta->at(idxLeadingJet), recoJet_phi->at(idxLeadingJet), 0. );
+		p4Jet2.SetPtEtaPhiM( recoJet_pt->at(idxSubLeadingJet).at(SYSJET), recoJet_eta->at(idxSubLeadingJet), recoJet_phi->at(idxSubLeadingJet), 0. );
+		TLorentzVector p4DiJet = p4Jet1 + p4Jet2;
+		_histograms1D.at("mJJSV").Fill( p4DiJet.M(), evtWeight );
+
+	}
 
 	return;
+
 }
